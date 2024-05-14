74ebdc8dc6aa0a467d4c1b12e6d107211c6abee8
Gabriel A Valladares Ruiz <157747016+GabeValla@users.noreply.github.com>
2024-05-14 15:38:34 -0500

    Create BlockLetter.cpp
    While doing computer science in school, I enrolled in additional courses to enhance my skills. During this program, I had the opportunity to create a block letter representation using my initials.

---
+++ BlockLetter.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+// Gabriel Valladares-Ruiz 1st
+using namespace std;
+//I was class president for 4 years straight 
+int main(){
+
+cout << " G G G     V     V" << endl;
+cout << "G       G  V     V"  << endl;
+cout << "G       G  V     V"<< endl;
+cout << "G          V     V" << endl;
+cout << "G G G G G  V     V" << endl;
+cout << "G       G   V   V  " << endl;
+cout << "  G G G       V" << endl;
+
+
+
+  return 0;
+}
